1d50890df785c38759d9d49359a305cac05c41a5
Mehmet Ali Tezcan <mehmetalitez33@gmail.com>
2025-04-09 21:33:00 +0300

    ilk

---
+++ src/Personel.java
@@ -0,0 +1,42 @@
+public class Personel {
+    private String firstName;
+    private String lastName;
+    private ContactInfo contactInfo;
+
+    public Personel(String firstName, String lastName, String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contactInfo = new ContactInfo(phone, email);
+    }
+
+    // İç sınıf (Inner Class)
+    public class ContactInfo {
+        private String phoneNumber;
+        private String email;
+
+        public ContactInfo(String phoneNumber, String email) {
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void displayContactInfo() {
+            System.out.println("Telefon: " + phoneNumber);
+            System.out.println("E-posta: " + email);
+        }
+    }
+
+    // Çalışanın tüm bilgilerini gösteren metot
+    public void displayEmployeeInfo() {
+        System.out.println("Ad: " + firstName);
+        System.out.println("Soyad: " + lastName);
+        contactInfo.displayContactInfo();
+    }
+}
